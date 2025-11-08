3150cf35face74248148f0c8986b253b31868c3d
Akshay Priyadarshi <akshayp1904@outlook.com>
2025-11-08 10:39:32 +0530

    updated tailwind and made to work with shadcn

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,22 @@
 import { defineConfig } from "vite";
 import svgr from "vite-plugin-svgr";
 import react from "@vitejs/plugin-react";
-import tsConfigPaths from "vite-tsconfig-paths";
+import tailwindcss from "@tailwindcss/vite";
+import path from "path";
 
 export default defineConfig({
   plugins: [
     svgr(),
+    tailwindcss(),
     react({
       babel: {
         plugins: [["babel-plugin-react-compiler", {}]],
       },
     }),
-    tsConfigPaths(),
   ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
